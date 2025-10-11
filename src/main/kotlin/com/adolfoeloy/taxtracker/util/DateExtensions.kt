bd8ba2a53a832b26c1d8c24c57e3833a26a49f38
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-10-11 13:07:26 +1100

    Add date extension functions for string conversion and last day calculation

--- src/main/kotlin/com/adolfoeloy/taxtracker/util/DateExtensions.kt
+++ src/main/kotlin/com/adolfoeloy/taxtracker/util/DateExtensions.kt
@@ -6,3 +6,13 @@ fun LocalDate.toYearMonthString(): String {
     val month = this.monthValue.toString().padStart(2, '0')
     return "${this.year}$month"
 }
+
+fun String.fromYearMonthString(): LocalDate {
+    val year = this.substring(0, 4).toInt()
+    val month = this.substring(4, 6).toInt()
+    return LocalDate.of(year, month, 1)
+}
+
+fun LocalDate.lastDay(): LocalDate {
+    return this.withDayOfMonth(this.lengthOfMonth())
+}
--- src/test/kotlin/com/adolfoeloy/taxtracker/util/DateExtensionsTest.kt
+++ src/test/kotlin/com/adolfoeloy/taxtracker/util/DateExtensionsTest.kt
@@ -20,4 +20,24 @@ class DateExtensionsTest {
         assertEquals("202301", result)
     }
 
+    @Test
+    fun `fromYearMonthString should return correct LocalDate`() {
+        val dateString = "202312"
+        val result = dateString.fromYearMonthString()
+        assertEquals(LocalDate.of(2023, 12, 1), result)
+    }
+
+    @Test
+    fun `fromYearMonthString should handle leading zero in month`() {
+        val dateString = "202301"
+        val result = dateString.fromYearMonthString()
+        assertEquals(LocalDate.of(2023, 1, 1), result)
+    }
+
+    @Test
+    fun `lastDay should return the last day of the month`() {
+        val date = LocalDate.of(2023, 2, 10)
+        val result = date.lastDay()
+        assertEquals(LocalDate.of(2023, 2, 28), result)
+    }
 }
